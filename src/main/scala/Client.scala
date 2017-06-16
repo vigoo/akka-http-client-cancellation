29259669cc1b4ef99ad28e1dea40eb0bace659e1
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2017-06-16 16:23:52 +0200

    Implementation without connection pool

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -1,47 +1,54 @@
 import akka.Done
 import akka.actor.ActorSystem
+import akka.pattern.after
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.HostConnectionPool
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, RequestTimeoutException, Uri}
-import akka.pattern.after
 import akka.stream.QueueOfferResult.{Dropped, Enqueued, QueueClosed}
-import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult, ThrottleMode}
 import akka.stream.scaladsl._
+import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult, ThrottleMode}
+import org.reactivestreams.{Subscriber, Subscription}
 
-import scala.concurrent.{Future, Promise}
 import scala.concurrent.duration._
+import scala.concurrent.{Future, Promise}
 import scala.util.{Failure, Success, Try}
 
 object Client extends App {
 
-  type Pool = Flow[(HttpRequest, (Future[Done], Promise[HttpResponse])), (Try[HttpResponse], (Future[Done], Promise[HttpResponse])), HostConnectionPool]
-  type Queue = SourceQueueWithComplete[(HttpRequest, (Future[Done], Promise[HttpResponse]))]
+  type Queue = SourceQueueWithComplete[(HttpRequest, Promise[HttpResponse])]
 
   implicit val actorSystem = ActorSystem("client")
   implicit val executionContext = actorSystem.dispatcher
   implicit val materializer = ActorMaterializer()
 
-  val connectionPool: Pool = Http().cachedHostConnectionPool("localhost", port = 8080)
+  val bufferSize = 8
+  val timeout = 190.millis
   val queue: Queue =
     Source
-      .queue[(HttpRequest, (Future[Done], Promise[HttpResponse]))](bufferSize = 8, overflowStrategy = OverflowStrategy.dropNew)
-      .filterNot { case (_, (timedOut, _)) =>
-          timedOut.isCompleted
-      }
-      .via(connectionPool)
-      .toMat(Sink.foreach {
-        case (Success(response), (_, promise)) => promise.success(response)
-        case (Failure(reason), (_, promise)) => promise.failure(reason)
+      .queue[(HttpRequest, Promise[HttpResponse])](bufferSize, overflowStrategy = OverflowStrategy.dropNew)
+      .toMat(Sink.foreach[(HttpRequest, Promise[HttpResponse])] {
+        case (request, responsePromise) =>
+          val connectionFlow = Http()
+            .outgoingConnection(request.uri.authority.host.toString(), request.uri.effectivePort)
+
+          val responseFuture = Source
+            .single(request)
+            .via(connectionFlow)
+            .idleTimeout(timeout)
+            .runWith(Sink.head)
+
+          responsePromise.tryCompleteWith(responseFuture)
       })(Keep.left)
       .run()
 
   def performRequest(n: Int): Unit = {
     println(s"$n: Sending request")
-    val request = HttpRequest(uri = Uri(s"/test/$n"))
+    val request = HttpRequest(uri = Uri(s"http://localhost:8080/test/$n"))
     val timedOut = Promise[Done]
     val responseFuture: Future[HttpResponse] = {
       val promise = Promise[HttpResponse]
-      Try(queue.offer(request -> (timedOut.future, promise))) match {
+      val now = System.currentTimeMillis()
+      Try(queue.offer(request -> promise)) match {
         case Success(offerResult) =>
           offerResult.flatMap {
             case Enqueued =>
@@ -58,23 +65,7 @@ object Client extends App {
       }
     }
 
-    val timeout = after(190.millis, actorSystem.scheduler) {
-      // We need to consume (discard) the response when it eventually arrives
-      // NOTE: Here we could should somehow cancel the whole request
-      responseFuture.onComplete {
-        case Success(response) =>
-          // Discarding succeeded timed out request
-          response.entity.discardBytes()
-        case Failure(_) =>
-        // Ignoring failed timed out request
-      }
-      timedOut.success(Done)
-      Future.failed[HttpResponse](RequestTimeoutException(request, "Request level completion timeout"))
-
-    }
-    val guardedResponse = Future.firstCompletedOf(Seq(responseFuture, timeout))
-
-    guardedResponse.onComplete {
+    responseFuture.onComplete {
       case Success(response) =>
         response.discardEntityBytes()
         println(s"$n: Got response")
